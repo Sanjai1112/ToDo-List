02096b40311c48616c61e7ba1acbc9d8291c4de0
Sanjai <sanjaikumar4773@gmail.com>
2019-01-29 18:35:12 +0530

    Updates

---
+++ precache-manifest.e21efeb5237158d182a475811e64b3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ee18833024ee3bf32a3",
+    "url": "/Sanjai1112/ToDo-List/static/js/runtime~main.1ee18833.js"
+  },
+  {
+    "revision": "8dd34330f390e3031d48",
+    "url": "/Sanjai1112/ToDo-List/static/js/main.8dd34330.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/Sanjai1112/ToDo-List/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "8dd34330f390e3031d48",
+    "url": "/Sanjai1112/ToDo-List/static/css/main.450d6a5a.chunk.css"
+  },
+  {
+    "revision": "e323c9ccce7b59dee967cec598e66d24",
+    "url": "/Sanjai1112/ToDo-List/index.html"
+  }
+];
